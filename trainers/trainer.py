f67f773ce2f17f1f9cf4e44e17a45e29cc6e2fe0
lqiqiqi <602500713@qq.com>
2019-08-06 15:57:43 +0800

    load model

--- trainers/trainer.py
+++ trainers/trainer.py
@@ -12,10 +12,10 @@ class Trainer(BaseTrain):
 
     def train(self):
 
-        # weigh initialization
+        #load model if model exists weigh initialization
         if self.config.load_model is True:
-            self.model.load_model()
             print('debug load model!!!!')
+            self.model.load_model()
         else:
             self.model.weight_init(mean=0.0, std=0.02)
             print('weight is initilized')
