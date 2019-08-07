e369fe8308a6d3ec9b79b6e7c4767da2a25b564d
lqiqiqi <602500713@qq.com>
2019-08-07 17:56:41 +0800

    L2 pena

--- trainers/trainer_plot_test.py
+++ trainers/trainer_plot_test.py
@@ -84,33 +84,13 @@ class Trainer(BaseTrain):
 
             # caculate test loss
             with torch.no_grad():
-                loss_test = 0
-                loss_log_test = 0
-                for input_test, target_test, groundtruth in test_data_loader:
-                    # input data (low resolution)
-                    if self.config.gpu_mode:
-                        x_test = Variable(input_test.cuda())
-                        y_test = Variable(groundtruth.cuda())
-                        y_log_test = Variable(target_test.cuda())
-                    else:
-                        x_test = Variable(input_test)
-                        y_test = Variable(groundtruth)
-                        y_log_test = Variable(target_test)
-
-                    # prediction
-                    model_out_test = self.model(x_test)
-
-                relog = torch.mul(torch.add(torch.exp(torch.mul(model_out_test, math.log(100))), -1), 1 / 100)
-
-                loss_test += torch.sqrt(self.MSE_loss(relog, y_test))  # RMSE for re-log result and original meter data
-                loss_log_test += torch.sqrt(self.MSE_loss(model_out_test, y_log_test))  # RMSE for log result # 结果得到是np.float
+                loss_test, loss_log_test = self.test(test_data_loader)
 
             epoch_loss_test = loss_test / len(test_data_loader)
             epoch_loss_log_test = loss_log_test / len(test_data_loader)
 
             avg_loss_test.append(float(epoch_loss_test))
             avg_loss_log_test.append(float(epoch_loss_log_test))
-
 
         # Plot avg. loss
         utils.plot_loss(self.config, [avg_loss, avg_loss_log_test])
@@ -123,6 +103,26 @@ class Trainer(BaseTrain):
 
         # Save final trained parameters of model
         self.model.save_model(epoch=None)
+
+    def test(self, test_data_loader):
+        loss_test = 0
+        loss_log_test = 0
+        for input_test, target_test, groundtruth in test_data_loader:
+            # input data (low resolution)
+            x_test = Variable(input_test)
+            y_test = Variable(groundtruth)
+            y_log_test = Variable(target_test)
+
+            # prediction
+            model_out_test = self.model(x_test)
+
+        relog = torch.mul(torch.add(torch.exp(torch.mul(model_out_test, math.log(100))), -1), 1 / 100)
+
+        loss_test += torch.sqrt(self.MSE_loss(relog, y_test))  # RMSE for re-log result and original meter data
+        loss_log_test += torch.sqrt(self.MSE_loss(model_out_test, y_log_test))  # RMSE for log result # 结果得到是np.float
+
+        return loss_test, loss_log_test
+
 
 
 
