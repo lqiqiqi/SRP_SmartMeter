20cc3fe3f2fb9c660bf3d8ef41a226612db82f50
lqiqiqi <602500713@qq.com>
2019-08-10 15:12:43 +0800

    load data fast

--- data_loader/data_generator.py
+++ data_loader/data_generator.py
@@ -48,7 +48,7 @@ class TxtDataset(Dataset):  # 这是一个Dataset子类
         highdata_log = torch.div(torch.log(torch.mul(highdata, 1000) + 1), math.log(100))
 
         # interpolate需要三维输入，先升为三维后，再squeeze降维
-        lowdata = torch.nn.functional.interpolate(np.expand_dims(highdata, 0), scale_factor=1 / self.config.scale_factor).squeeze(0)
+        lowdata = torch.nn.functional.interpolate(highdata.unsqueeze(0), scale_factor=1 / self.config.scale_factor).squeeze(0)
         lowdata_log = torch.div(torch.log(torch.mul(lowdata, 1000) + 1), math.log(100))
 
         return lowdata_log, highdata_log, highdata  # 返回txt, label, groundtruth
