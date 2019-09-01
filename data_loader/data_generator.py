b0f80dee90e1fae01ddce1b4b932affa6ec9a335
lqiqiqi <602500713@qq.com>
2019-09-01 17:55:34 +0800

    liu

--- data_loader/data_generator.py
+++ data_loader/data_generator.py
@@ -55,9 +55,7 @@ class TxtDataset(Dataset):  # 这是一个Dataset子类
         # interpolate需要三维输入，先升为三维后，再squeeze降维
         # lowdata = torch.nn.functional.interpolate(highdata.unsqueeze(0), scale_factor=1 / self.config.scale_factor).squeeze(0)
 
-        lowdata = torch.take(highdata, torch.arange(0, highdata.size()[1], self.config.scale_factor).long())
-        print(highdata.size())
-        print(lowdata.size())
+        lowdata = torch.take(highdata, torch.arange(0, highdata.size()[1], self.config.scale_factor).long()).unsqueeze(0)
         lowdata_log = torch.div(torch.log(torch.mul(lowdata, 1000) + 1), math.log(100))
 
         return lowdata_log, highdata_log, highdata  # 返回txt, label, groundtruth
