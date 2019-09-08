31aba61fb70a8eb9d30f15ebf3f59161da1be789
lqiqiqi <602500713@qq.com>
2019-09-08 11:00:27 +0800

    test liu postlog

--- trainers/trainer.py
+++ trainers/trainer.py
@@ -164,16 +164,16 @@ class Tester(BaseTrain):
     def load_spec_model(self):
         model_dir = os.path.join(self.config.save_dir, 'model_' + self.config.exp_name)
 
-        model_name = model_dir + '/' + self.config.model_name + '_param_epoch_180.pkl'  # get specific model
+        model_name = model_dir + '/' + self.config.model_name + '_param_epoch_55.pkl'  # get specific model
         if os.path.exists(model_name):
             state_dict = torch.load(model_name)
-            # from collections import OrderedDict
-            # new_state_dict = OrderedDict()
-            # for k, v in state_dict.items():
-            #     namekey = k[7:]  # remove `module.`
-            #     new_state_dict[namekey] = v
-            # self.model.load_state_dict(new_state_dict)
-            self.model.load_state_dict(state_dict)
+            from collections import OrderedDict
+            new_state_dict = OrderedDict()
+            for k, v in state_dict.items():
+                namekey = k[7:]  # remove `module.`
+                new_state_dict[namekey] = v
+            self.model.load_state_dict(new_state_dict)
+            # self.model.load_state_dict(state_dict)
             print('Trained generator model is loaded.')
             return True
         else:
